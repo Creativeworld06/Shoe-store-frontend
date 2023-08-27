b01b29238264c6f45b12dab2ae5fa151a354fa32
Hira Arooj <charleswalles2006@gmail.com>
2023-08-27 19:24:40 +0500

    first commit

--- pages/cart.js
+++ pages/cart.js
@@ -84,12 +84,12 @@ console.log(error)
              </div>
              
              {/* BUTTON START */}
-             <button className='w-full py-4 rounded-full bg-black text-white text-lg font-medium transition-transform active:scale-95 mb-3 hover:opacity-75' onClick={handlePayment}>
+             <button className='w-full py-4 rounded-full bg-black text-white text-lg font-medium transition-transform active:scale-95 mb-3 hover:opacity-75 flex item-center gap-2 justify-center' onClick={handlePayment}>
               Checkout
               {loading && <img src="/spinner.svg" /> }
              </button>
              {/* BUTTON END */}
-            </div>
+            </div> 
             {/* SUMMARY END */}
           </div>
           {/* CART CONTENT END */}
